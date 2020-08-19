9d5329fd8ed1a3129f20bee5057820f5cf263153
vaibz96 <vshanbha@stevens.edu>
2020-08-19 04:21:08 -0400

    React Js app setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <h1> Let us build my food cart app on reactjs</h1>
       <h2> Hello Vaibhav </h2>
-      <h3> Hello again testing new push</h3>
     </div>
   );
 }
