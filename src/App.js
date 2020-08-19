871b687cb193ff280a50bec4fed62f916c8b2526
vaibz96 <vshanbha@stevens.edu>
2020-08-19 01:28:17 -0400

    added routes to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Let us build my food cart app on reactjs</h1>
-      <h2> Hello Vaibhav </h2>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1> Login Page </h1>
+          </Route>
+          {/* this is the default home page */}
+          <Route path="/">
+            <h1> Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
